a27a59e06462085a38928c3070c7ef0a73c21616
Abhishek kumar <abhishek.kumar@avalara.com>
2023-11-17 17:23:45 +0530

    Dockerise

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -5,13 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+var temp = builder.Configuration["IdentityServiceUrl"];
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
        .AddJwtBearer(options =>
        {
-           options.Authority = builder.Configuration["IdentityServiceUrl"];
-           options.RequireHttpsMetadata = false;
-           options.TokenValidationParameters.ValidateAudience = false;
-           options.TokenValidationParameters.NameClaimType = "username";
+              options.Authority = builder.Configuration["IdentityServiceUrl"];
+              options.RequireHttpsMetadata = false;
+              options.TokenValidationParameters.ValidateAudience = false;
+              options.TokenValidationParameters.NameClaimType = "username";
        });
 var app = builder.Build();
 
